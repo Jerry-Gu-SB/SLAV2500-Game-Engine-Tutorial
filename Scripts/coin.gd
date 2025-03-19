ca5c56c961016609c35bcfb22b3982c3e3e8d8c3
Jerry Gu <jerrysonggu@gmail.com>
2025-03-19 16:45:13 -0400

    coin sound added? haven't tested

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 @onready var animation = $AnimatedSprite2D
-
+@onready var coin_sound = $AudioStreamPlayer2D
+var is_collected = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,7 +15,9 @@ func _process(_delta: float) -> void:
 
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and not is_collected:
+		is_collected = true
+		coin_sound.play()
 		self.queue_free()
 		body.score += 1
 		print(body.score)
